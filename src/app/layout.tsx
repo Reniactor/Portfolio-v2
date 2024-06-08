9c3fb75b906fefc8f44da40a7789530d16895487
Reniactor <reniactor@hotmail.com>
2024-06-07 20:47:31 -0500

    Meta description added. OpenGraph included

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 
 export const metadata = {
+  openGraph: {
+    title: "Arquímedes Vásquez | Software Developer in Bogotá",
+    description: "Fullstack software developer based in Bogotá, Colombia",
+    url: "https://arquimedes-vasquez.vercel.app",
+    siteName: "Arquímedes Vásquez Portfolio",
+    images: [
+      {
+        url: "https://prnt.sc/Tb_W00bKBrgp",
+        width: 600,
+        height: 250,
+        alt: "Website banner",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
   title: "Arquímedes Vásquez | Software Developer in Bogotá",
   description: "Fullstack software developer based in Bogotá, Colombia",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
